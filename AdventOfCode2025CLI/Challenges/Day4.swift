e6ddeb96e388f36f1872886d29e8cc5d3f08e996
Simon Salomons <simon.salomons@icloud.com>
2025-12-04 08:05:55 +0100

    Day 4

--- AdventOfCode2025CLI/Challenges/Day4.swift
+++ AdventOfCode2025CLI/Challenges/Day4.swift
@@ -8,5 +8,43 @@
 import Foundation
 
 func day4() {
-    let input = content(file: "input3")
+    let input = content(file: "input4")
+
+    var grid = input.grid()
+
+    func removeRolls() -> Int {
+        var rolls: Set<Pos> = []
+
+        grid.loop { pos, element in
+            if element == "@" {
+                var rollCount = 0
+                for dir in Direction.all {
+                    if grid.at(pos + dir) == "@" {
+                        rollCount += 1
+                    }
+                }
+                if rollCount < 4 {
+                    rolls.insert(pos)
+                }
+            }
+        }
+
+        for roll in rolls {
+            grid.set(".", at: roll)
+        }
+
+        return rolls.count
+    }
+
+    var count = removeRolls()
+    var totalCount = count
+
+    print("Part 1:", count) // 1370
+
+    while count > 0 {
+        count = removeRolls()
+        totalCount += count
+    }
+
+    print("Part 2:", totalCount) // 8437
 }
--- AdventOfCode2025CLI/main.swift
+++ AdventOfCode2025CLI/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 let startTime = DispatchTime.now()
 
-day3()
+day4()
 
 let endTime = DispatchTime.now()
 let elapsed = Duration.nanoseconds(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds)
