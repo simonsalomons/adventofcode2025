a15bac68d42dd1d01b97ab0af325eb3be4f86a9f
Simon Salomons <simon.salomons@icloud.com>
2025-12-11 10:31:40 +0100

    Day 11

--- AdventOfCode2025CLI/Challenges/Day11.swift
+++ AdventOfCode2025CLI/Challenges/Day11.swift
@@ -7,4 +7,59 @@
 
 func day11() {
     let input = content(file: "input11")
+
+    var devices: [String: [String]] = [:]
+    for line in input.lines() {
+        let c = line.split(separator: ":")
+        let name = String(c[0])
+        let outputs = c[1].split(separator: " ", omittingEmptySubsequences: true).map({ String($0) })
+        devices[name] = outputs
+    }
+
+    struct CacheKey: Hashable {
+        let device: String
+        let destination: String
+        let ignore: String?
+    }
+
+    var cache: [CacheKey: Int] = [:]
+    func getPathCount(from device: String, to destination: String, ignore: String? = nil) -> Int {
+        guard device != "out" else { return 0 }
+
+        let cacheKey = CacheKey(device: device, destination: destination, ignore: ignore)
+
+        if let cached = cache[cacheKey] {
+            return cached
+        }
+
+        let outputs = devices[device]!
+
+        var count = 0
+        for output in outputs {
+            if output != destination {
+                if let ignore {
+                    guard output != ignore else { continue }
+                }
+                count += getPathCount(from: output, to: destination, ignore: ignore)
+            } else {
+                count += 1
+            }
+        }
+        cache[cacheKey] = count
+        return count
+    }
+
+    print("Part 1:", getPathCount(from: "you", to: "out")) // 543
+
+    let svrToDac = getPathCount(from: "svr", to: "dac", ignore: "fft")
+    let svrToFft = getPathCount(from: "svr", to: "fft", ignore: "dac")
+    let dacToFft = getPathCount(from: "dac", to: "fft")
+    let fftToDac = getPathCount(from: "fft", to: "dac")
+    let dacToOut = getPathCount(from: "dac", to: "out")
+    let fftToOut = getPathCount(from: "fft", to: "out")
+
+    let svrDacFftCount = svrToDac * dacToFft * fftToOut
+    let svrFftDacCount = svrToFft * fftToDac * dacToOut
+
+    print("Part 2:", svrDacFftCount + svrFftDacCount) // 479511112939968
 }
--- AdventOfCode2025CLI/main.swift
+++ AdventOfCode2025CLI/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 let startTime = DispatchTime.now()
 
-day10()
+day11()
 
 let endTime = DispatchTime.now()
 let elapsed = Duration.nanoseconds(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds)
